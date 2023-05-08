8093343ff065bcdd6aa9ed7809a959d0f3027f72
Lalith Kishore <91070157+Lalith-prabhaharan@users.noreply.github.com>
2023-05-08 14:22:08 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -11,7 +11,7 @@ export const Home = () => {
     <div>
       <NavBar/>
       <div className='containe'>
-    <img src="https://wallpaperaccess.com/full/1470814.jpg"></img>
+    <img src="https://c4.wallpaperflare.com/wallpaper/105/484/965/towards-the-sunset-white-airplane-wallpaper-preview.jpg"></img>
     <div className="text-overlay">
     <p className='s-heading'>Use Code <b>FLYHIGH</b> for Flat 10% Discount</p>
     <button  onClick={view} className="home-but"> View Flights!!</button>
